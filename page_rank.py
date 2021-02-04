7e8ad923a3c9e67f7f2437a7fd27780558d7cdbb
unknown <b9053803@newcastle.ac.uk>
2021-02-04 14:05:43 +0200

    completed project

--- page_rank.py
+++ page_rank.py
@@ -1,6 +1,10 @@
 import os
 import time
 from progress import Progress
+import networkx as nx
+import random
+
+
 
 WEB_DATA = os.path.join(os.path.dirname(__file__), 'school_web.txt')
 
@@ -22,16 +26,19 @@ def load_graph(fd):
     Each line in the file contains two white space seperated URLs and
     denotes a directed edge (link) from the first URL to the second.
     """
+    graph = nx.DiGraph()
     # Iterate through the file line by line
     for line in fd:
         # And split each line into two URLs
         node, target = line.split()
-        raise RuntimeError("This function is not implemented yet.")
+        graph.add_edge(node, target)
+    return graph
 
 
 def print_stats(graph):
         """Print number of nodes and edges in the given graph"""
-        raise RuntimeError("This function is not implemented yet.")
+        print("Number of nodes:", graph.number_of_nodes())
+        print("Number of edges:", graph.number_of_edges())
 
 
 def stochastic_page_rank(graph, n_iter=1_000_000, n_steps=100):
@@ -49,7 +56,18 @@ def stochastic_page_rank(graph, n_iter=1_000_000, n_steps=100):
     a random walk that starts on a random node will after n_steps end
     on each node of the given graph.
     """
-    raise RuntimeError("This function is not implemented yet.")
+    # Puts all graph nodes in a set and attaches 0 as their current hit count
+    hit_count = {}
+    for x in graph.nodes:
+        hit_count[x] = 0
+
+    # Iterates through random nodes and their out edges and calculates their hit count
+    for i in range(n_iter):
+        current_node = random.choice(list(graph.nodes))
+        for n in range(n_steps):
+            current_node = random.choice(list(graph.neighbors(current_node)))
+        hit_count[current_node] += 1/n_iter
+    return hit_count
 
 
 def distribution_page_rank(graph, n_iter=100):
@@ -65,8 +83,25 @@ def distribution_page_rank(graph, n_iter=100):
     This function estimates the Page Rank by iteratively calculating
     the probability that a random walker is currently on any node.
     """
-    raise RuntimeError("This function is not implemented yet.")
+    # Puts all graph nodes in a set and attaches their initial probabilities
+    node_prob = {}
+    for x in graph.nodes:
+        node_prob[x] = 1 / graph.number_of_nodes()
 
+    for i in range(n_iter):
+        # Initializing next probability as 0
+        next_prob = {}
+        for node in graph.nodes:
+            next_prob[node] = 0
+
+        # The probability of having been at the current node is divided by the number of it's out edges
+        # and then the value is added to the probability of being at the target node in the next step
+        for node in graph.nodes:
+            p = node_prob[node]/graph.out_degree(node)
+            for target in graph.neighbors(node):
+                next_prob[target] += p
+        node_prob = next_prob
+    return node_prob
 
 def main():
     # Load the web structure from file
